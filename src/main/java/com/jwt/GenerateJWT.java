14e4cbb7dbd7469daba512db6d68029830e2b3e0
Fazal Mohammed <fazal10@gmail.com>
2018-11-16 22:04:21 -0500

    Added classes for filtering urls

--- src/main/java/com/jwt/GenerateJWT.java
+++ src/main/java/com/jwt/GenerateJWT.java
@@ -1,51 +1,73 @@
 package com.jwt;
 
 import java.util.Calendar;
+import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 
 @RestController
+@ConfigurationProperties(prefix = "app")
 public class GenerateJWT {
 	
+	private String secret;
+	private String issuer;
+	
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	public String getIssuer() {
+		return issuer;
+	}
+
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+	
+    @RequestMapping("/")
+    public ModelAndView displayArticle(Map<String, Object> model) {
+
+        model.put("title", "hello");
+
+        return new ModelAndView("index", model);
+    }
+	
 	@RequestMapping("/generateToken")
-	public String generateToken() {
+	public ModelAndView generateToken(Map<String, Object> model, HttpServletResponse response) {
 		try {
 			Calendar cal = Calendar.getInstance();
-			cal.add(Calendar.SECOND, 50);
-		    Algorithm algorithm = Algorithm.HMAC256("app-secret");
+			cal.add(Calendar.SECOND, 5);
+		    Algorithm algorithm = Algorithm.HMAC256(this.secret);
 		    String token = JWT.create()
-		        .withIssuer("app")
+		        .withIssuer(this.issuer)
 		        .withExpiresAt(cal.getTime())
 		        .sign(algorithm);
-		    return token;
+		    response.addCookie(new Cookie("token", token));
+		    model.put("title", token);
+		    return new ModelAndView("index", model);
 		} catch (JWTCreationException exception){
 			exception.printStackTrace();
-			return exception.toString();
+			return null;
 		}
 	}
 	
-	@RequestMapping("/validateToken")
-	public String validateToken(@RequestParam(name="token") String token) {
-		try {
-		    Algorithm algorithm = Algorithm.HMAC256("app-secret");
-		    JWTVerifier verifier = JWT.require(algorithm)
-		        .withIssuer("app")
-		        .build(); //Reusable verifier instance
-		    DecodedJWT jwt = verifier.verify(token);
-		    return jwt.toString();
-		} catch (JWTVerificationException exception){
-			exception.printStackTrace();
-			return exception.toString();
-		}
+	@RequestMapping("/ajax/validateToken")
+	public String validateToken() {
+		return "token is valid";
 	}
 
 }
---
+++ src/main/java/com/jwt/TokenFilter.java
@@ -0,0 +1,89 @@
+package com.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@WebFilter(urlPatterns = "/ajax/*")
+@ConfigurationProperties(prefix = "app")
+public class TokenFilter implements Filter {
+	
+	private JWTVerifier verifier;
+	private String secret;
+	private String issuer;
+	
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	public String getIssuer() {
+		return issuer;
+	}
+
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		Filter.super.init(filterConfig);
+	    Algorithm algorithm = Algorithm.HMAC256(this.secret);
+	    this.verifier = JWT.require(algorithm)
+	        .withIssuer(this.issuer)
+	        .build();
+	}
+ 	
+	@Override
+	public void doFilter(ServletRequest request, javax.servlet.ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		boolean isTokenValidBoolean = false;
+        HttpServletRequest req = (HttpServletRequest) request;
+        
+        if(req.getCookies() != null) {
+	        for(Cookie c: req.getCookies()) {
+	        	System.out.println(c.getName() + " -> " + c.getValue());
+	        	if(c.getName().equals("token")) {
+	        		isTokenValidBoolean = this.isTokenValid(c.getValue());
+	        		System.out.println("token validity => " + this.isTokenValid(c.getValue()));
+	        	}
+	        }
+        }
+        
+		if(isTokenValidBoolean){
+			chain.doFilter(request, response);
+		} else {
+			 ((HttpServletResponse) response).sendRedirect("/");
+		}
+
+	}
+	
+	private boolean isTokenValid(String token) {
+		try {
+		    this.verifier.verify(token);
+		    return true;
+		} catch (JWTVerificationException exception){
+			//exception.printStackTrace();
+			return false;
+		}
+	}
+ 
+}
